ba683fe81b2496ef7a3e5885b4a80984b555d7e8
egor43 <egor43.rus@mail.ru>
2016-10-31 00:47:05 +0300

    Version 1.0 Доделана отрисовка прямоугольников, номеров и соединительных линий. Протестировано на тестовом примере.

--- TreeViewerWPF/TreeViewerWPF/MainWindow.xaml.cs
+++ TreeViewerWPF/TreeViewerWPF/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -26,15 +27,18 @@ namespace TreeViewerWPF
     {
         public Node Left, Right;
         public int Number;
+        public int LengthTree;
+        public int DepthTree;
+        public int MaxWidthTree;
     }
 
     public partial class MainWindow : Window
     {
         public Node Tree;
-         
+
         public MainWindow()
         {
-            
+
             InitializeComponent();
         }
 
@@ -48,23 +52,59 @@ namespace TreeViewerWPF
             FileStream fs = new FileStream(OpenFile.FileName, FileMode.Open);
             Tree = (Node)formatter.Deserialize(fs);
             fs.Close();
-            PrintTree(Tree);
+            Tree.MaxWidthTree = Convert.ToInt32(Math.Pow(2, Tree.DepthTree = HeightTree(Tree)));
+            Tree.LengthTree = CountTree(Tree);
+            PrintTree(Tree,60*Tree.MaxWidthTree/2, 0, 60 * Tree.MaxWidthTree / 2);
         }
 
-        public void PrintTree(Node tree)
+        public void PrintTree(Node tree, double StartX, double StartY, double center)
         {
-            GeometryGroup group = new GeometryGroup();
-            for (int i = 0; i < 10; i++)
+            double minus = center/2;
+            RectangleGeometry rect = new RectangleGeometry();
+            rect.Rect = new Rect(StartX, StartY, 50, 40);
+            TextBlock tb = new TextBlock();
+            tb.Text = Convert.ToString(tree.Number);
+            tb.Margin = new Thickness(StartX, StartY, 0, 0);
+            System.Windows.Shapes.Path pt = new System.Windows.Shapes.Path();
+            pt.Fill = Brushes.LightBlue;
+            pt.Data = rect;
+            grid.Children.Add(pt);
+            grid.Children.Add(tb);
+            if (tree.Left != null)
             {
-                RectangleGeometry rect = new RectangleGeometry();
-                rect.Rect=new Rect(10,20,30,40);
-                System.Windows.Shapes.Path pt = new System.Windows.Shapes.Path();
-                pt.Fill = Brushes.LightBlue;
-                pt.Data = rect;
-                canvas.Children.Add(pt);
+                LineGeometry line = new LineGeometry();
+                line.StartPoint = new Point(StartX+25, StartY+40);
+                line.EndPoint = new Point(StartX - minus+25, StartY + 50+40);
+                System.Windows.Shapes.Path ptline = new System.Windows.Shapes.Path();
+                ptline.Stroke=Brushes.Black;
+                ptline.Data = line;
+                grid.Children.Add(ptline);
+                PrintTree(tree.Left, StartX - minus, StartY + 50, minus);
             }
-            
+            if (tree.Right != null)
+            {
+                LineGeometry line = new LineGeometry();
+                line.StartPoint = new Point(StartX + 25, StartY + 40);
+                line.EndPoint = new Point(StartX + minus + 25, StartY + 50 + 40);
+                System.Windows.Shapes.Path ptline = new System.Windows.Shapes.Path();
+                ptline.Stroke = Brushes.Black;
+                ptline.Data = line;
+                grid.Children.Add(ptline);
+                PrintTree(tree.Right, StartX + minus, StartY + 50, minus);
+            }
+         
+        }
 
+        public int HeightTree(Node tree)
+        {
+            if (tree == null) return -1;
+            return Math.Max(HeightTree(tree.Left), HeightTree(tree.Right)) + 1;
+        }
+
+        public int CountTree(Node tree)
+        {
+            if (tree == null) return 0;
+            return (CountTree(tree.Left) + CountTree(tree.Right)) + 1;
         }
     }
 }
